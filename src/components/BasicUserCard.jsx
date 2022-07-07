f9b3de009f6a4a9de52b0997f311ddb185fe9779
omthakur1224 <omthakur1224@gmail.com>
2022-07-07 14:44:39 +0530

    first page

--- src/components/BasicUserCard.jsx
+++ src/components/BasicUserCard.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getData, storeData } from '../redux/action';
 import { Store } from '../redux/store';
+import { Input, Space } from 'antd';
+// import TextField from '@material-ui/core/TextField';
+// import Autocomplete from '@material-ui/lab/Autocomplete'
 // import axios from 'axios'
 import './BasicUserDetail.css'
 function BasicUserCard() {
-    
+    const { Search } = Input;
+    const onSearch = (value) => console.log(value);
     const dispatch=useDispatch();
 
     const data=useSelector(Store=>Store.data)
@@ -29,12 +33,31 @@ function BasicUserCard() {
     <div>
 
         <h1>Rick and Morty Search</h1>
+        {/* <i className="fa fa-instagram icon"> */}
+{/*        
+    <input style={{ "width":"300px",
+                    // "margin":"auto",
+                    "height":"40px",
+                //    "boxShadow": "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                    }} type="search" /> */}
 
-        <input type="text" />
+                        {/* </i> */}
+                       {/* <Search
+                            placeholder="input search text"
+                            onSearch={onSearch}
+                            style={{
+                                "width": "200px",
+                            }}
+    /> */}
+        <div className="wrapper">
+            <img className="icon" src="https://cdn0.iconfinder.com/data/icons/TWG_Retina_Icons/64/magnifier.png" alt="" />
+            <input placeholder='search for a contact' className="search" type='search'></input>
+        </div> 
+
 
         <div style={{"display":"flex",
          "flexDirection":"column",
-         "gap":"5px",
+         "gap":"3px",
         //  "width":"50%",
         //  "margin":"auto"
          }}>
